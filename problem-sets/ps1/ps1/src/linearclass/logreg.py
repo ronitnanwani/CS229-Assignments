2375aa5a70e5fb4f4842117977e0ea70195ab193
Ronit Nanwani <ronit.nanwani@gmail.com>
2022-12-03 18:46:06 +0530

    Implemented Logistic Regression using Newton's Method

--- problem-sets/ps1/ps1/src/linearclass/logreg.py
+++ problem-sets/ps1/ps1/src/linearclass/logreg.py
@@ -1,6 +1,10 @@
 import numpy as np
 import util
+import math
 
+def h_theta(theta,x):
+    p=np.dot(theta,x)
+    return(1/(1+pow(math.e,-p)))
 
 def main(train_path, valid_path, save_path):
     """Problem: Logistic regression with Newton's Method.
@@ -16,6 +20,22 @@ def main(train_path, valid_path, save_path):
     # Train a logistic regression classifier
     # Plot decision boundary on top of validation set set
     # Use np.savetxt to save predictions on eval set to save_path
+
+    clf=LogisticRegression()
+    clf.fit(x_train,y_train)
+
+    x_eval,y_eval=util.load_dataset(valid_path,add_intercept=True)
+    plot_path=save_path.replace('.txt','.png')
+    util.plot(x_eval,y_eval,clf.theta,plot_path)
+
+    p_eval=clf.predict(x_eval)
+    np.savetxt(save_path,p_eval)
+
+    cnt=0
+    for i in range(x_eval.shape[0]):
+        if((p_eval[i]>=0.5 and y_eval[i]==1) or (p_eval[i]<=0.5 and y_eval[i]==0)):
+            cnt+=1
+    print("Logistic Regression accuracy is: ",(100.0*cnt)/(x_eval.shape[0]),"%")
     # *** END CODE HERE ***
 
 
@@ -51,6 +71,25 @@ class LogisticRegression:
             y: Training example labels. Shape (n_examples,).
         """
         # *** START CODE HERE ***
+        n=x.shape[0]
+        dim=x.shape[1]
+        self.theta=np.zeros(dim)
+        prev_theta=np.ones(dim)
+        iteration=0
+        while(iteration<self.max_iter and math.sqrt(np.dot(self.theta-prev_theta,self.theta-prev_theta))>self.eps):
+
+            grad=np.zeros(dim)
+            for i in range(n):
+                grad=grad+(y[i]-h_theta(self.theta,x[i]))*x[i]
+            
+            hessian=np.zeros((dim,dim))
+            for i in range(n):
+                hessian=hessian-(h_theta(self.theta,x[i]))*(1-h_theta(self.theta,x[i]))*(np.outer(x[i],x[i]))
+            hessian_inv=np.linalg.inv(hessian)
+
+            prev_theta=self.theta
+            self.theta=self.theta-self.step_size*hessian_inv.dot(grad)
+            iteration+=1
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -63,13 +102,17 @@ class LogisticRegression:
             Outputs of shape (n_examples,).
         """
         # *** START CODE HERE ***
+        p_eval=[]
+        for i in range(x.shape[0]):
+            p_eval.append(h_theta(self.theta,x[i]))
+        p_eval=np.array(p_eval)
+        return p_eval
         # *** END CODE HERE ***
 
 if __name__ == '__main__':
     main(train_path='ds1_train.csv',
          valid_path='ds1_valid.csv',
          save_path='logreg_pred_1.txt')
-
     main(train_path='ds2_train.csv',
          valid_path='ds2_valid.csv',
          save_path='logreg_pred_2.txt')
